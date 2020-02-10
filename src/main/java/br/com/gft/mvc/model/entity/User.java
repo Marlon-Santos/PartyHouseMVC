228ea760f53df3d9b55b98feda736dcdc689e936
Marlon Santos <marlonergondossantos@gmail.com>
2020-02-09 23:12:08 -0300

    create user entity

--- src/main/java/br/com/gft/mvc/model/entity/User.java
+++ src/main/java/br/com/gft/mvc/model/entity/User.java
@@ -1,11 +1,13 @@
 package br.com.gft.mvc.model.entity;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import java.util.Objects;
 
+@Entity
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) @NotNull
