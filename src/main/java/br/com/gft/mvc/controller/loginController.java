abf7be10fa4f0259fb267fec7616cdd9c563cfa9
marlon-santos <marlonergondossantos@gmail.com>
2020-02-12 15:54:51 -0300

    login implements

--- src/main/java/br/com/gft/mvc/controller/loginController.java
+++ src/main/java/br/com/gft/mvc/controller/loginController.java
@@ -1,4 +1,12 @@
 package br.com.gft.mvc.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class loginController {
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
 }
