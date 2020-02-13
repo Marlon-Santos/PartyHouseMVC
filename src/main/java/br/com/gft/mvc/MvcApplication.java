6c2cccf391b236db2d07451e950bf0508947e06c
marlon-santos <marlonergondossantos@gmail.com>
2020-02-13 16:06:57 -0300

    bcrypt test

--- src/main/java/br/com/gft/mvc/MvcApplication.java
+++ src/main/java/br/com/gft/mvc/MvcApplication.java
@@ -2,12 +2,15 @@ package br.com.gft.mvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class MvcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MvcApplication.class, args);
+		System.out.println(new BCryptPasswordEncoder().encode("123"));
 	}
 
 }
