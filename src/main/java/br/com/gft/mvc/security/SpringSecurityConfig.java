20cc4e8aa0a128fe75efddf424ac7079b1d7bcbd
marlon-santos <marlonergondossantos@gmail.com>
2020-02-13 16:04:37 -0300

    security's config

--- src/main/java/br/com/gft/mvc/security/SpringSecurityConfig.java
+++ src/main/java/br/com/gft/mvc/security/SpringSecurityConfig.java
@@ -1,12 +1,18 @@
-package br.com.gft.mvc.config;
+package br.com.gft.mvc.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
-public class Security extends WebSecurityConfigurerAdapter {
+class Security extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserDetailsService userDetailsService;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
@@ -18,8 +24,7 @@ public class Security extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .withUser("admin").password("{noop}admin").roles("ADMIN");
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(new BCryptPasswordEncoder());
     }
 }
