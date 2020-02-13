9e6cc1a388684eedcdf8beaa86abbf1e865fc1a2
marlon-santos <marlonergondossantos@gmail.com>
2020-02-13 16:03:55 -0300

    add login controller

--- src/main/java/br/com/gft/mvc/controller/loginController.java
+++ src/main/java/br/com/gft/mvc/controller/loginController.java
@@ -1,7 +1,14 @@
 package br.com.gft.mvc.controller;
 
+import br.com.gft.mvc.model.entity.User;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.security.auth.login.LoginContext;
+
 
 @Controller
 public class loginController {
