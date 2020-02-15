9eb8be445103234bdc2cbcd76e7ac44591f45333
Marlon Santos <marlonergondossantos@gmail.com>
2020-02-14 23:17:20 -0300

    create loginCadastro methos

--- src/main/java/br/com/gft/mvc/controller/loginController.java
+++ src/main/java/br/com/gft/mvc/controller/loginController.java
@@ -1,10 +1,15 @@
 package br.com.gft.mvc.controller;
 
 import br.com.gft.mvc.model.entity.User;
+import br.com.gft.mvc.model.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
 import javax.security.auth.login.LoginContext;
@@ -12,8 +17,23 @@ import javax.security.auth.login.LoginContext;
 
 @Controller
 public class loginController {
+    @Autowired
+    private UserRepository userRepository;
     @GetMapping("/login")
     public String login() {
         return "login";
+    }
+    @GetMapping("/loginCadastro")
+    public ModelAndView user(){
+        ModelAndView mv = new ModelAndView("loginCadastro");
+        return mv;
+    }
+    @PostMapping("/loginCadastro")
+    public RedirectView newUser(User user){
+        String bCrypt = new BCryptPasswordEncoder().encode(user.getPassword());
+        System.out.println(bCrypt+">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"+user.getLogin());
+        user.setPassword(bCrypt);
+        userRepository.save(user);
+        return new RedirectView("http://localhost:8080/login");
     }
 }
--- src/main/java/br/com/gft/mvc/security/SpringSecurityConfig.java
+++ src/main/java/br/com/gft/mvc/security/SpringSecurityConfig.java
@@ -2,14 +2,20 @@ package br.com.gft.mvc.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
+@Configuration
 class Security extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsService userDetailsService;
@@ -24,7 +30,6 @@ class Security extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService)
-                .passwordEncoder(new BCryptPasswordEncoder());
+        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
     }
 }
